f38bb57221c118b59ac6ef706fc9816727a661bb
maggie <mdyslin@mcad.edu>
2022-01-27 20:52:48 -0600

    Updating movies & creating second exercise

--- weektwo/exercises/exerciseone/exerciseone.js
+++ weektwo/exercises/exerciseone/exerciseone.js
@@ -1,3 +1,10 @@
-let faveMovies = ['Lord of the Rings', 'Jurassic Park', 'Jaws', 'The Little Mermaid', 'A Goofy Movie'];
+let faveMovies = ['Lord of the Rings', 'Jurassic Park', 'Jaws', 'Alien & Aliens', 'A Goofy Movie'];
+// console.log(faveMovies);
 
-console.log(faveMovies);
+const userChoice = window.prompt('What is your favorite movie?');
+// console.log('So your favorite movie is ' + movie)
+
+faveMovies.push(userChoice);
+// console.log('Favorite movies include ' + faveMovies + ', ' + userChoice);
+
+console.log('Our favorite movies include: ', faveMovies.toString());
