108031ebe8e83a991f5ffb3239280518b621b7f7
maggie <mdyslin@mcad.edu>
2022-02-03 21:02:55 -0600

    making the tile work with shapes!

--- weekthree/exercises/one/main.js
+++ weekthree/exercises/one/main.js
@@ -4,7 +4,29 @@ function setup() {
 }
 
 function squareTile() {
+    translate(0, 0);
+    fill(60, 165, 207);
+    noStroke();
+    rect(0, 0, 200);
 
+    // large circle
+    stroke('purple');
+    strokeWeight(8);
+    circle(100, 100, 192);
+
+    fill('purple');
+    noStroke();
+    // x1, y1, x2, y2, x3, y3
+    triangle(50, 75, 150, 75, 100, 160);
+
+    // top circle
+    circle(100, 40, 45);
+
+    // left circle
+    circle(40, 125, 45);
+
+    // right circle
+    circle(160, 125, 45);
 }
 
 function draw() {
