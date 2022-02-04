d81ca0ed45598dc44a04d1be2cd9a8c1098c392e
maggie <mdyslin@mcad.edu>
2022-02-03 21:09:50 -0600

    copy + paste

---
+++ weekthree/exercises/two/main.js
@@ -0,0 +1,34 @@
+function setup() {
+    createCanvas(200, 200);
+    noLoop();
+}
+
+function squareTile() {
+    translate(0, 0);
+    fill(232, 214, 255);
+    noStroke();
+    rect(0, 0, 200);
+
+    // large circle
+    stroke(134, 0, 255);
+    strokeWeight(8);
+    circle(100, 100, 192);
+
+    fill(134, 0, 255);
+    noStroke();
+    // x1, y1, x2, y2, x3, y3
+    triangle(50, 75, 150, 75, 100, 160);
+
+    // top circle
+    circle(100, 40, 45);
+
+    // left circle
+    circle(40, 125, 45);
+
+    // right circle
+    circle(160, 125, 45);
+}
+
+function draw() {
+    squareTile();
+}
