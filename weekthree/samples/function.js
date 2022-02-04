42c0051f96bcf26bcb9fe23266bb099f5bafabb2
maggie <mdyslin@mcad.edu>
2022-02-04 12:02:23 -0600

    working on sample.

--- weekthree/samples/function.js
+++ weekthree/samples/function.js
@@ -1,5 +1,23 @@
-function logOutGreeting() { // logOutGreeting is the name you choose; can be ANYTHING
-    console.log('Hi');
+function logOutGreeting(greeting) { // logOutGreeting is the name you choose; can be ANYTHING
+    console.log(greeting);
 }
 
-logOutGreeting(); // executing the function by using the same name you chose
+logOutGreeting("hi"); // executing the function by using the same name you chose
+
+// function to get the makings of a pb sammich
+
+function getStuff (ingredient) {
+    console.log('get ' + ingredient);
+}
+
+getStuff('bread');
+getStuff('peanut butter');
+getStuff('honey');
+
+const makeSandwich = function (ingredientArray) {
+    for (let i = 0; i < ingredientArray.length; i++) {
+        console.log('get ', ingredientArray[i]);
+    }
+}
+
+makeSandwich(['bread', 'mayo', 'tunafish']);
