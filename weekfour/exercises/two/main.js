a6ecffb8c73394caf46b4d5f2917b7b38147cc07
maggie <mdyslin@mcad.edu>
2022-02-10 19:03:06 -0600

    exercise 2

---
+++ weekfour/exercises/two/main.js
@@ -0,0 +1,34 @@
+let rectX = 0;
+const rectHeight = 75;
+let rectY;
+const rectWidth = 75;
+let clickCount = 0;
+let speed;
+
+function setup() {
+    createCanvas(500, 500);
+    rectY = random(height - rectHeight); // chooses any number from the height of our canvas & 0
+    speed = random(1, 3);
+}
+
+function draw() {
+    background("lightblue");
+    drawShape();
+    rectX+= speed;
+    if (rectX > width) {
+        noLoop();
+        text('Your score was ' + clickCount, 100, 300);
+    }
+}
+
+function mousePressed() {
+    if ((mouseX >= rectX && mouseX <= rectX + rectWidth) && (mouseY >= rectY && mouseY <= rectY + rectHeight)) {
+        clickCount++; // same as clickCount += 1
+        console.log('hit', clickCount);
+    }
+}
+
+function drawShape() {
+    fill("black");
+    rect(rectX, rectY, rectWidth, rectHeight);
+}
