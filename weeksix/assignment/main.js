d6b93ac3107e21262f070f2effd7339dcf01de60
maggie <mdyslin@mcad.edu>
2022-02-26 17:58:14 -0600

    Adding a Llama image & facts about the llamas.

--- weeksix/assignment/main.js
+++ weeksix/assignment/main.js
@@ -1,8 +1,19 @@
 const vm = new Vue({
     el: "#deLlama", // selecting element
     data: {
-      message: "A Llama",
-      secondMessage: "This is so cool!",
+      llamaArray: [
+          "A Llama's lifespan is 20 years, though some live up to 30.",
+          "Llamas live in herds, and will 'adopt' groups of sheep or goats as their own herd.",
+          "Llamas protect their herds by chasing off predators like Coyotes.",
+          "Llamas can grow as tall as 6 feet.",
+          "Llamas can hum, which is one way they can communicate.",
+          "Llamas are used as therapy animals, much like dogs. Many say Llamas have a 'soothing aura'.",
+          "Yarn made from Llama's undercoat is extremely soft and warm. It is used in clothing and baby blankets.",
+          "Llamas outer coats are used for rugs and rope.",
+          "Llamas can shoot green spit up to 10 feet away. They do this to establish a pecking order within their herd.",
+          "Llama spit is sometimes green due to undigested food.",
+          "*BONUS* Llamas are vegetarians and have highly effecient digestive systems."
+        ],
       anAlpaca: "yes"
     }
   });
