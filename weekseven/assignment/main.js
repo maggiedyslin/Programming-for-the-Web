53ccae995504c11d5f9055a81d1e20c5702a1a94
maggie <mdyslin@mcad.edu>
2022-03-04 16:01:04 -0600

    Creating objects for the array & walking through video.

---
+++ weekseven/assignment/main.js
@@ -0,0 +1,71 @@
+let vm = new Vue({
+    el: "#myCollection",
+    data: {
+        gameCollection: [
+            {
+                name: "The Legend of Zelda: Ocarina of Time",
+                released: "November 21, 1998",
+                console: "Nintendo 64, Nintendo GameCube, and Nintendo 3DS",
+                rating: "10/10",
+                players: "1",
+                description: "The Legend of Zelda: Ocarina of Time is a fantasy action-adventure game set in an expansive environment. The player controls series protagonist Link from a third-person perspective in a three-dimensional world. Link primarily fights with a sword and shield, but can also use other weapons such as projectiles, bombs, and magic spells."
+            },
+            {
+                name: "Final Fantasy X",
+                released: "July 19, 2001",
+                console: "PlayStation 2, PlayStation 3, and Playstation 4",
+                rating: "10/10",
+                players: "1",
+                description: "Final Fantasy X tells the story of a star blitzball player, Tidus, who journeys with a young and beautiful summoner named Yuna on her quest to save the world of Spira from an endless cycle of destruction wrought by the colossal menace Sin."
+            },
+            {
+                name: "Pokemon Gold",
+                released: "November 21, 1999",
+                console: "Gameboy Color",
+                rating: "10/10",
+                players: "1",
+                description: "Pokémon Gold Version (and Pokémon Silver Version) are 1999 role-playing video games developed by Game Freak and published by Nintendo for the Game Boy Color. They are the first installments in the second generation of the Pokémon video game series."
+            },
+            {
+                name: "Paper Mario: The Thousand-Year Door",
+                released: "July 22, 2004",
+                console: "Nintendo GameCube",
+                rating: "10/10",
+                players: "1",
+                description: "The Thousand-Year Door is a role-playing video game (RPG) with other nontraditional RPG elements. The player controls a two-dimensional version of Mario and explores a variety of worlds designed to look like paper. In these locales, he is tasked with retrieving seven Crystal Stars which involves Mario completing puzzles and defeating enemies to proceed."
+            },
+            {
+                name: "Horizon Zero Dawn",
+                released: "February 28, 2017",
+                console: "PlayStation 4",
+                rating: "10/10",
+                players: "1",
+                description: "Horizon Zero Dawn is an action role-playing game played from a third-person view. Players take control of Aloy, a hunter who ventures through a post-apocalyptic land ruled by robotic creatures. Aloy can kill enemies in a variety of ways (setting traps such as tripwires using the Tripcaster, shooting them with arrows, using explosives, and a spear). Machine components, including electricity and the metal they are composed of, are vital to Aloy's survival; she can loot their remains for crafting resources. Ammo, resource satchels, pouches, quivers, resistance, antidotes, health potions, and traps can all be crafted. Weapons have modification slots for dealing more damage. Aloy wears a Focus, a small head piece that scans machines to determine their susceptibilities, identify their location, their level, and the nature of loot they will drop."
+            },
+            {
+                name: "Resident Evil 7",
+                released: "January 24, 2017",
+                console: "PlayStation 4, Xbox One, and PC",
+                rating: "9/10",
+                players: "1",
+                description: "The player controls Ethan Winters from a first-person perspective as he searches a derelict estate for his missing wife. Although Ethan is a civilian with few combat skills, he is able to arm himself with a variety of weapons including handguns, shotguns, flamethrowers, explosives and chainsaws against the estate's residents, the Baker family, as well as humanoid fungal creatures known as the 'molded' and mutated insects."
+            },
+            {
+                name: "The Evil Within 2",
+                released: "October 13, 2017",
+                console: "PlayStation 3, PlayStation 4, Xbox 360, Xbox One, and PC",
+                rating: "8/10",
+                players: "1",
+                description: "Similar to its predecessor, the game is a survival horror game. Played from a third-person perspective, the player assumes control of detective Sebastian Castellanos, who must descend into the world of Union to rescue his daughter, Lily. "
+            },
+            {
+                name: "Jurassic World Evolution",
+                released: "June 12, 2018",
+                console: "PlayStation 4, Xbox One",
+                rating: "7/10",
+                players: "1",
+                description: "Jurassic World Evolution is a business simulation game that allows the player to construct a Jurassic World dinosaur theme park with attractions and research facilities. Players must build an Expedition Center, which sends paleontologists to fossil dig sites to obtain dinosaurs' DNA material. DNA sequencing, which can be done in the Fossil Center unlocks new dinosaurs and updates their statistics, such as lifespan and resilience. With enough DNA content, players can use the Hammond Creation Lab to breed and incubate dinosaurs. Players can also improve the dinosaurs' genes by integrating DNA from modern species with that of the dinosaurs to fill their gaps and allow them to evolve. Modifications to the dinosaurs' DNA change their base statistics, as well as everything from their level of aggressiveness to their appearance. The game features a terrain tool which allows players to modify the environment by planting trees and creating water sources."
+            }
+        ]
+    }
+})
