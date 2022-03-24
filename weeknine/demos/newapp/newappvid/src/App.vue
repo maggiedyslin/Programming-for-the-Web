02b238be9f5198277856c6248ca621855fe0a075
maggie <mdyslin@mcad.edu>
2022-03-24 15:32:34 -0500

    vue application walkthrough

---
+++ weeknine/demos/newapp/newappvid/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+  const isWorking = true;
+
+</script>
+
+<template>
+
+<div class="container">
+
+  <h1>My second compiled Vue App</h1>
+    
+    <div v-if="isWorking">
+
+      <p>it is working</p>
+
+    </div>
+
+</div>
+
+</template>
+
+<style>
+
+.container {
+  width: 600px;
+  margin: 20px auto 0;
+  text-align: center;
+  font-size: 18px;
+}
+
+h1 {
+  font-size: 40px;
+}
+
+</style>
