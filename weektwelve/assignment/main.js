48bebc828725b63a6e424c541c83591560b588cb
maggie <mdyslin@mcad.edu>
2022-04-18 19:21:50 -0500

    dunununnun

--- weektwelve/assignment/main.js
+++ weektwelve/assignment/main.js
@@ -11,18 +11,35 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(1000, 365);
+    // createCanvas(1000, 365);
+    let cnv = createCanvas(1000, 400);
     cnv.mousePressed(canvasPressed);
 
+    background('#016bc1');
     image(sharkImg, 0, 0);
 }
 
 function draw() {
-    line(width / 2, 0, width / 2, 365)
+    strokeWeight(3);
+    stroke(255);
+    line(width / 2, 365, width / 2, 400);
+
+    noStroke();
+    fill(255);
+    textSize(15)
+    text('Tap this side of the image to play the first note of the Jaws movie!', 25, 390);
+    text('Tap this side of the image to play the second note of the Jaws movie!', 525, 390)
 }
 
 function canvasPressed() {
-    if () {
-
+    if ((mouseX <= 500) && (mouseY <= 365)) {
+        pianoE.play();
+    } else if ((mouseX >= 500) && (mouseY <= 365)) {
+        pianoF.play();
     }
+}
+
+function mouseReleased() {
+    pianoE.stop();
+    pianoF.stop();
 }
