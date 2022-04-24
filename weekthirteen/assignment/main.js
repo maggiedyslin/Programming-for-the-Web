ee3e064d6aaa5d5483aa29257f82a06db19019b2
maggie <mdyslin@mcad.edu>
2022-04-24 13:19:28 -0500

    updating - trying to make rain

--- weekthirteen/assignment/main.js
+++ weekthirteen/assignment/main.js
@@ -1,8 +1,27 @@
+let ellipseX;
+const ellipseHeight = 30;
+let ellipseY = 100;
+const ellipseWidth = 5;
+let speed;
+
 function setup() {
     createCanvas(1000, 800);
-    background(0);
+    ellipseX = random(height - ellipseHeight);
+    speed = random(1, 5);
 }
 
 function draw() {
+    background(0);
+    drawShape();
+    ellipseY += speed;
+    if (ellipseY > height) {
+        noLoop();
+    }
+}
 
+function drawShape() {
+    // for (let i = 0; i <= 10; i++) {
+        fill(255);
+        ellipse(ellipseX, ellipseY, 5, 30);
+    // }
 }
