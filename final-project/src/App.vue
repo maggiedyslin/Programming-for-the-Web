827e3d12f4f0608009ce3e42294263bf77fb12fe
maggie <mdyslin@mcad.edu>
2022-05-03 22:11:17 -0500

    updating various items
    updating ul>li tags
    updating width, margin & padding for various items
    importing bebas neue font
    updating width of container/main

--- final-project/src/App.vue
+++ final-project/src/App.vue
@@ -11,11 +11,16 @@
 
             <nav>
 
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/blue">Blue</RouterLink>
-                <RouterLink to="/shirts">Shirts</RouterLink>
-                <RouterLink to="/species">Species</RouterLink>
-                <RouterLink to="/information">Information</RouterLink>
+                <img src="/src/assets/deep-blue-logo.svg" alt="Deep Blue Logo">
+
+                <ul>
+                    <li><RouterLink to="/">Home</RouterLink></li>
+                    <li><RouterLink to="">About</RouterLink></li>
+                    <li><RouterLink to="/blue">Blue</RouterLink></li>
+                    <li><RouterLink to="/shirts">Shirts</RouterLink></li>
+                    <li><RouterLink to="/species">Species</RouterLink></li>
+                    <li><RouterLink to="/information">Information</RouterLink></li>
+                </ul>
 
             </nav>
 
@@ -31,15 +36,51 @@
 
 <style>
 
+    @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+
+    body {
+        padding: 0;
+        margin: 0;
+    }
+
     .container {
-        width: 100%;
+        max-width: 100%;
+        margin: 0 auto;
     }
 
     nav {
-        padding: 20px;
-        /* margin-top: 20px; */
+        font-family: 'Bebas Neue';
+        font-size: 50px;
+        padding: 10px 0 0;
         top: 100px;
+    }
+
+    nav img {
+        display: block;
+        max-width: 20%;
+        margin: 0 auto;
+        padding: 15px 0;
+    }
+
+    nav ul, nav ul li {
         background-color: #B4E5E0;
+        list-style-type: none;
+        display: flex;
+        flex-direction: row;
+        margin: 0;
+        padding: 0;
+        justify-content: space-between;
+    }
+
+    nav ul li a {
+        text-decoration: none;
+        color: #325C7E;
+        padding: 5px 25px 0;
+    }
+
+    nav ul li a:hover {
+        color: #325C7E;
+        background-color: white;
     }
 
     body {
@@ -48,8 +89,12 @@
 
     main {
         background-color: #325C7E;
-        width: 1000px;
+        top: 0;
+        width: 100%;
+        max-width: 1100px;
         margin: 0 auto;
+        padding: 0;
+        border: 2px solid black;
     }
 
 </style>
